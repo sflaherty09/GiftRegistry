ba0deb90dc0c7b8a8f2c9f3b379725d47c612044
sflaherty09 <sflahert@ramapo.edu>
2018-04-22 22:03:26 -0400

    added friend requests

--- GiftRegistry/Controllers/FriendsController.cs
+++ GiftRegistry/Controllers/FriendsController.cs
@@ -6,6 +6,16 @@ using System.Web.Mvc;
 using System.Data;
 using System.Data.Entity;
 using GiftRegistry.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Net;
+using GiftRegistry.Controllers;
+
+using SendGrid.Helpers;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace GiftRegistry.Controllers
 {
@@ -13,13 +23,23 @@ namespace GiftRegistry.Controllers
     {
 
         private ApplicationDbContext db = new ApplicationDbContext();
+
+        private IdentityDbContext db3 = new IdentityDbContext();
+
+        private ApplicationUser db2 = new ApplicationUser();
+
+        private ApplicationUserManager UserManager;
+
+
         // GET: Friends
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
         // GET: AddFriend
+        [Authorize]
         public ActionResult AddFriend()
         {
             var users = from u in db.Users
@@ -28,9 +48,68 @@ namespace GiftRegistry.Controllers
             return View(users.ToList());
         }
 
-        /*
-         *            var gifts = from g in db.GiftLists
-                        select g;
-         */
+        [HttpGet]
+        public ActionResult ConfirmRequest(string id)
+        {
+            //var user = db3.Users.Find(id);
+
+            
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+
+            var user = db.Users.Find(id);
+
+            //ApplicationUser au = db.Users.Find(id);
+
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: ConfirmRequest
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmRequest(string id, string formValue)
+        {
+
+            if (formValue == "No")
+            {
+                return RedirectToAction("AddFriend");
+            }
+
+            var client = new SendGridClient("You API Key");
+
+            // Always delete the previous line and put this comment in instead or your account will get suspended
+
+            var user = db.Users.Find(id);
+
+            string friendEmail = user.Email;
+
+            var fromUser = db.Users.Find(User.Identity.GetUserId());
+
+            string message = fromUser.Name + " would like to be your friend. Click HERE to accept";
+
+
+            var msg = new SendGrid.Helpers.Mail.SendGridMessage()
+            {
+                From = new EmailAddress("friends@gifts.com", fromUser.Name),
+                Subject = "Friend Request",
+                PlainTextContent = message,
+                HtmlContent = "<strong>" + message + "</strong>"
+            };
+            msg.AddTo(new EmailAddress(friendEmail));
+
+            client.SendEmailAsync(msg);
+
+
+            return RedirectToAction("AddFriend");
+        }
     }
 }
